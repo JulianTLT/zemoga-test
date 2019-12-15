91dc2c242f2befbe42cef366979bf7d4e7641983
Julian <sjulian9409@gmail.com>
2019-12-14 20:52:24 -0500

    Initial commit

---
+++ src/main/java/com/zemoga/zemogatest/ZemogaTestApplication.java
@@ -0,0 +1,13 @@
+package com.zemoga.zemogatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZemogaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZemogaTestApplication.class, args);
+	}
+
+}
